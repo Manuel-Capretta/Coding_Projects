37cd997db60a6d3d650dfbe49f72bddec370aaab
Your Name <familiecapretta@gmail.com>
2021-05-11 09:27:03 +0200

    Co-authored-by: Jonas-Heuberger <Jonas-Heuberger@users.noreply.github.com>

--- C Projects/Other Algorythms/Comparing_Two_Arrays.c
+++ C Projects/Other Algorythms/Comparing_Two_Arrays.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int main(){
   int a[] = {1,1,2,3,5,8,13,21,34,55,89};
   int b[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-  int i = 0;
-  int j = 0;
-  while (j < sizeof(b)) {
-    int aNumber = a[i];
-    if (a[i] == b[j]) {
-      printf("%da %d", a[i], b[j]);
-      i++;
+  int countA = 0;
+  int countB = 0;
+  while (countA < sizeof(a)){
+    if(a[countA] == b[countB]){
+      printf("%d %d", a[countA], b[countB]);
+      countA++;
     } else {
-      j++;
+      countB++;
     }
   }
 }
