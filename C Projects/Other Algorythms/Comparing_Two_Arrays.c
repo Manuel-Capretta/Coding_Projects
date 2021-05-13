2e62ffa037c030d938eb55bc578965bd9716cd38
Your Name <familiecapretta@gmail.com>
2021-05-13 11:02:44 +0200

    main

--- C Projects/Other Algorythms/Comparing_Two_Arrays.c
+++ C Projects/Other Algorythms/Comparing_Two_Arrays.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-  int a[] = {1,1,2,3,5,8,13,21,34,55,89};
-  int b[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+  int a[] = {1,1,2,3,5,8};
+  int b[] = {1,2,3,4,5,6,7,8,};
   int countA = 0;
   int countB = 0;
   while (countA < sizeof(a)){
