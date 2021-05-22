6f902924e2c8fc86dbc6ebf6b32d3d0785112293
Your Name <familiecapretta@gmail.com>
2021-05-22 17:21:51 +0200

    main

--- Discord_Bot/Bot.py
+++ Discord_Bot/Bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 Client = commands.Bot(command_prefix='--') #Get the command bot. Set prfic to '--'
+tokenClient = discord.Client()
 
 @Client.command(name='version')
 async def version(context):
@@ -10,15 +11,18 @@ async def version(context):
   versionEmbed.add_field(name="Version Code:", value="Python 3.10", inline=False)
   versionEmbed.add_field(name="Bot Code:", value="1.0", inline=False)
   versionEmbed.set_footer(text="Versions do change from time to time")
-  versionEmbed.set_author(name="My-first-bot")
+  versionEmbed.set_author(name=" ")
 
   #Send embeded message
   await context.message.channel.send(embed=versionEmbed)
 
-@Client.event
-async def on_connect(message):
-  if message.author == Client.user:
-    return
-  await message.channel.send('Hello!')
+@tokenClient.event
+async def on_message(message):
+   if message.content.startswith('pls traps'):
+    await message.channel.send('No traps plz')
+
+@tokenClient.event
+async def on_connect():
+   print("Bot ready")
   
-Client.run('TOKEN') #token is renewed
+Client.run('TOKEN') #token
