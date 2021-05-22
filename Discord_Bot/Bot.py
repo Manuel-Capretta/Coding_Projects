273cfb6be1d5085884415b250ca8ce08698ecdb1
Your Name <familiecapretta@gmail.com>
2021-05-22 17:38:18 +0200

    main

--- Discord_Bot/Bot.py
+++ Discord_Bot/Bot.py
@@ -11,8 +11,17 @@ async def version(context):
   versionEmbed.add_field(name="Bot Code:", value="1.0", inline=False)
   versionEmbed.set_footer(text="Versions do change from time to time")
   versionEmbed.set_author(name=" ")
-
   #Send embeded message
   await context.message.channel.send(embed=versionEmbed)
+
+@Client.command(name='Author')
+async def Author(context):
+  #Create embeded message
+  authorEmbed = discord.Embed(title="Author information", description=" ", color=0xff0000)
+  authorEmbed.add_field(name="Author name", value="Manuel Capretta", inline=False)
+  authorEmbed.add_field(name="Author discord", value="MC_2914#2113", inline=False)
+  authorEmbed.set_footer(text="Nothing else to show here ;)")
+   #Send embeded message
+  await context.message.channel.send(embed=authorEmbed)
 
 Client.run('TOKEN') #token
