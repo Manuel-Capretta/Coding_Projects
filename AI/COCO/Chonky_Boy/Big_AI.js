6f561c83cd847c0f7a37ee2b5cd4dc032c7bf9bf
Your Name <familiecapretta@gmail.com>
2021-06-28 23:03:01 +0200

    mian

--- AI/COCO/Chonky_Boy/Big_AI.js
+++ AI/COCO/Chonky_Boy/Big_AI.js
@@ -51,11 +51,11 @@ function draw() {
   for (let i = 0; i < detections.length; i++) {
     let object = detections[i];
     if (object.label == "person") {
-      /*stroke(255, 0, 0);
+      stroke(255, 0, 0);
       strokeWeight(4);
       noFill();
       rect(object.x, object.y, object.width, object.height);
-      noStroke();
+      /*noStroke();
       fill(255);
       textSize(24);
       text(object.label, object.x + 10, object.y + 24);
