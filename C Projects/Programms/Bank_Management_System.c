85ea06e21df80be5322010b3210c58bd9915645b
Your Name <familiecapretta@gmail.com>
2021-07-07 12:08:18 +0200

    main

--- C Projects/Programms/Bank_Management_System.c
+++ C Projects/Programms/Bank_Management_System.c
@@ -4,7 +4,27 @@
 #include <stdbool.h>
 #include <string.h>
 
+//Var
+int balance;
+int pin;
+char name[20];
+char surname[20];
+
+//Transaction struct
+struct transaction {
+  int amount;
+  char reason[100];
+};
+
+//Func
+void acc();
+
 int main(void){
-  
+  acc();
+
   return 0;
+}
+
+void acc(){
+  printf("------------ Bank Management System ------------\n");
 }
