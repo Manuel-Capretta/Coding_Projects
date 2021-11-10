da78c8d8ebf712cee08737a69de0e39e25f8a063
Manuel Capretta <familiecapretta@gmail.com>
2021-11-11 00:16:33 +0100

    dnu

--- C Projects/test_programms/arrayChecker.c
+++ C Projects/test_programms/arrayChecker.c
@@ -6,30 +6,30 @@
 #include <string.h>
 
 int main(void){
-  int num = 22;
+  int num = 9;
   int arr[] = {2, 5, 7, 15, 3, 4};
   int solArr[2]; 
   bool stopIt_getSomeHelp = false; //breaks both loops
 
   /*Your Code*/
   for(int i = 0; i < sizeof(arr); i++){
-    for(int j = i+1; j < sizeof(arr); j++){
-      if (arr[i] + arr[j] == num) {
-        solArr[0] = arr[i];
-        solArr[1] = arr[j];
-        printf("\n%d + %d = %d", solArr[0], solArr[1], num);
-        stopIt_getSomeHelp = true;
-      } 
-    }
-    //If num isn't in there
-    if(i == 5){
-      printf("\nAin't in there mate");
-      stopIt_getSomeHelp = true;
-    }
     //Breaks both loops
     if(stopIt_getSomeHelp){
       break;
+    } else {
+      for(int j = i+1; j < sizeof(arr); j++){
+        if (arr[i] + arr[j] == num) {
+          solArr[0] = arr[i];
+          solArr[1] = arr[j];
+          printf("\n%d + %d = %d", solArr[0], solArr[1], num);
+          stopIt_getSomeHelp = true;
+        } 
+      }
+      //If num isn't in there
+      if(i == 5){
+        printf("\nAin't in there mate");
+        stopIt_getSomeHelp = true;
+      }
     }
   }
-
 }
